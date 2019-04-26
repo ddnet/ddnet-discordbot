5796659395dd6ee40552f724818b0a418a2d7373
12pm <30786226+12pm@users.noreply.github.com>
2019-04-26 15:35:17 +0200

    Add $eval

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import textwrap
 import traceback
+from contextlib import redirect_stdout
+from io import StringIO
 
 import discord
 from discord.ext import commands
 
+CONFIRM = '\N{OK HAND SIGN}'
 
 class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self._last_result = None
 
 
     async def cog_check(self, ctx: commands.Context) -> bool:
@@ -23,7 +28,7 @@ class Admin(commands.Cog):
         except Exception:
             await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         else:
-            await ctx.message.add_reaction('👌')
+            await ctx.message.add_reaction(CONFIRM)
 
 
     @commands.command()
@@ -33,7 +38,7 @@ class Admin(commands.Cog):
         except Exception:
             await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         else:
-            await ctx.message.add_reaction('👌')
+            await ctx.message.add_reaction(CONFIRM)
 
 
     @commands.command()
@@ -43,7 +48,63 @@ class Admin(commands.Cog):
         except Exception:
             await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         else:
-            await ctx.message.add_reaction('👌')
+            await ctx.message.add_reaction(CONFIRM)
+
+
+    def cleanup_code(self, content: str) -> str:
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+
+    @commands.command(name='eval')
+    async def _eval(self, ctx: commands.Context, *, body: str) -> None:
+        env = {
+            'self': self,
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as exc:
+            return await ctx.send(f'```py\n{exc.__class__.__name__}: {exc}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction(CONFIRM)
+            except discord.HTTPException:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
 
 def setup(bot: commands.Bot) -> None:
