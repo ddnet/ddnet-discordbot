c2d06b09a06bde73b9e1e31616b50bb2de58d4b2
12pm <30786226+12pm@users.noreply.github.com>
2020-04-04 19:30:15 +0200

    Cleanup

--- utils/menu.py
+++ utils/menu.py
@@ -9,25 +9,25 @@ class Pages(menus.Menu):
         super().__init__(clear_reactions_after=True)
 
         self.pages = pages
+        self.current_page = 0
         self.num_pages = len(pages)
 
     def should_add_reactions(self) -> bool:
         return self.num_pages > 1
 
-    def partial_message(self, page: int) -> Dict:
+    def partial_message(self) -> Dict:
         return {
-            'content': f'*Page {page + 1}/{self.num_pages}*',
-            'embed': self.pages[page]
+            'content': f'*Page {self.current_page + 1}/{self.num_pages}*',
+            'embed': self.pages[self.current_page]
         }
 
     async def send_initial_message(self, ctx: commands.Context, channel: discord.TextChannel) -> discord.Message:
-        self.current_page = page = 0
-        message = self.partial_message(page)
+        message = self.partial_message()
         return await ctx.send(**message)
 
     async def update_page(self, step: int):
-        self.current_page = page = (self.current_page + step) % self.num_pages
-        message = self.partial_message(page)
+        self.current_page = (self.current_page + step) % self.num_pages
+        message = self.partial_message()
         await self.message.edit(**message)
 
     @menus.button('◀️')
