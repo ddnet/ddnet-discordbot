7ee5f3bc45ef7d8edb37315b69685fa052018854
12pm <30786226+12pm@users.noreply.github.com>
2020-06-20 21:45:32 +0200

    better error handling

--- cogs/github.py
+++ cogs/github.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import enum
+import logging
 import re
+from datetime import datetime
 
 import discord
 from discord.ext import commands
+
+log = logging.getLogger(__name__)
 
 CHAN_DEVELOPER = 293493549758939136
 
@@ -30,20 +34,37 @@ class GithubException(Exception):
     pass
 
 
-class GitHubBase():
+class GithubRatelimit(GithubException):
+    def __init__(self, reset: int):
+        self.reset = reset
+        self.timestamp = datetime.utcfromtimestamp(reset)
+
+        message = f'Currently rate limited until {self.timestamp} UTC'
+        super().__init__(message)
+
+
+class GithubBase():
 
     bot = None
 
     async def _fetch(self, url: str) -> dict:
         headers = {'Accept:': 'application/vnd.github.v3+json, application/vnd.github.antiope-preview+json'}
         async with self.bot.session.get(f'https://api.github.com/{url}', headers=headers) as resp:
-            if resp.status != 200:
+            js = await resp.json()
+            if resp.status == 200:
+                return js
+            elif resp.status == 403:
+                reset = int(resp.headers['X-Ratelimit-Reset'])
+                log.warning('We are being rate limited until %s', datetime.fromtimestamp(reset))
+                raise GithubRatelimit(reset)
+            elif resp.status == 404:
                 raise RuntimeError
+            else:
+                log.error('Failed fetching %r from Github API: %s (status code: %d %s)', url, js['message'], resp.status, resp.reason)
+                raise GithubException('Failed fetching Github API')
 
-            return await resp.json()
 
-
-class Commit(GitHubBase):
+class Commit(GithubBase):
     def __init__(self, owner: str='ddnet', repo: str='ddnet', ref: str='master'):
         self.url = f'repos/{owner}/{repo}/commits/{ref}/check-suites'
 
@@ -76,7 +97,7 @@ class Commit(GitHubBase):
             return BuildStatus.SUCCESS
 
 
-class Issue(GitHubBase):
+class Issue(GithubBase):
     def __init__(self, owner: str, repo: str, id: str):
         self.owner = owner
         self.repo = repo
@@ -107,11 +128,15 @@ class Issue(GitHubBase):
 
 class Github(commands.Cog):
     def __init__(self, bot: commands.Bot):
-        self.bot = GitHubBase.bot = bot
+        self.bot = GithubBase.bot = bot
+        self.ratelimit = GithubRatelimit(0)
+
+    def ratelimited(self) -> bool:
+        return self.ratelimit.reset >= datetime.utcnow()
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.id != CHAN_DEVELOPER or (message.content and message.content[0] == self.bot.command_prefix) or message.author.bot:
+        if message.channel.id != CHAN_DEVELOPER or (message.content and message.content[0] == self.bot.command_prefix) or message.author.bot or self.ratelimited():
             return
 
         matches = re.finditer(_ISSUE_RE, message.content)
@@ -130,13 +155,20 @@ class Github(commands.Cog):
     @commands.command(usage='[commit]')
     async def build_status(self, ctx: commands.Context, commit: Commit=Commit()):
         """Show the build status of a PR/commit"""
+        if self.ratelimited():
+            return await ctx.send(self.ratelimit)
+
         status = await commit.get_status()
         await ctx.send(status)
 
     @build_status.error
     async def build_status_error(self, ctx: commands.Context, error: commands.CommandError):
-        if isinstance(error.original, GithubException):
-            await ctx.send(error.original)
+        original = error.original
+        if isinstance(original, GithubException):
+            await ctx.send(original)
+
+        if isinstance(original, GithubRatelimit):
+            self.ratelimit = original
 
 
 def setup(bot: commands.bot):
