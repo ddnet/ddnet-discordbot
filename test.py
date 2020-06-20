095c826f873a7f4742d21b2dfb3b8970a350402c
12pm <30786226+12pm@users.noreply.github.com>
2020-06-20 15:10:50 +0200

    .

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-issue_re = r'(?:(?P<owner>\w*)/)?(?P<repo>\w*)#(?P<id>\d+)\b'
-
-matches = re.finditer(issue_re, '12pm/ddnet-discordbot#1')
-for match in matches:
-    print(match.groupdict())
