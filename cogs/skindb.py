f6c8e4b1c7a5aea46444a2e509943b66d5f40b94
murpii <vNikyu@gmail.com>
2023-09-18 16:00:02 +0200

    Make submission message structure case-sensitive

--- cogs/skindb.py
+++ cogs/skindb.py
@@ -57,12 +57,13 @@ def check_attachment_amount(message: discord.Message):
 def check_message_structure(message: discord.Message):
     # Regex to make licenses optional:
     # "^(?P<skin_name>['\"].+['\"]) by (?P<creator_name>.+?)( (\((?P<license>CC0|CC-BY|CC-BY-SA)\)))?$"gm
-    regex = re.compile(r"^\"(?P<skin_name>.+)\" by (?P<user_name>.+) (\((?P<license>.{3,8})\))$", re.IGNORECASE)
+    regex = re.compile(r"^\"(?P<skin_name>.+)\" by (?P<user_name>.+) (\((?P<license>.{3,8})\))$")
     re_match = regex.match(message.content)
     if not re_match:
         return (False,
                 (f'- Your message isn\'t properly formatted. Follow the message structure written in <#986941590780149780>. '
-                'Also keep in mind licenses are now required for every submission.'), 'Bad message structure')
+                'Also keep in mind licenses are now required for every submission and proper uppercase and lowercase formatting is important as well.'),
+                'Bad message structure')
 
     licenses = ["CC0", "CC BY", "CC BY-SA"]
     if re_match.group('license'):
