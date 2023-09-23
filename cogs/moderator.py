25f0e60d7a2593e7d65d278673330a7664f639e0
murpii <vNikyu@gmail.com>
2023-09-24 00:29:29 +0200

    moderator: renamed a couple of things, clean-up

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import logging
 import re
 import discord
-
 from discord.ext import commands
 
-log = logging.getLogger(__name__)
 
 GUILD_DDNET      = 252358080522747904
 CHAN_DEV         = 293493549758939136
 CHAN_WIKI        = 871738312849752104
-forum_channel_id = 1147027590356414565
+FORUM_CHANNEL    = 1147027590356414565
 
 
 class Moderator(commands.Cog):
@@ -20,19 +17,19 @@ class Moderator(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener('on_message')
-    async def linkfilter(self, message: discord.Message):
+    async def spam_link_filter(self, message: discord.Message):
 
         if message.guild is None or message.guild.id != GUILD_DDNET or message.channel.id not in (CHAN_DEV, CHAN_WIKI):
             return
 
-        linkpattern = re.compile(r"https?:\/\/(www\.)?t\.me", re.IGNORECASE)
-        if linkpattern.search(message.content):
+        link_pattern = re.compile(r'https?:\/\/(www\.)?t\.me', re.IGNORECASE) # noqa
+        if link_pattern.search(message.content):
             await message.delete()
 
 
     @commands.Cog.listener()
     async def on_thread_create(self, thread):
-        if thread.parent_id == forum_channel_id:
+        if thread.parent_id == FORUM_CHANNEL:
             # thread.starting_message doesn't work, so I just simply use history instead
             async for msg in thread.history(limit=1, oldest_first=True):
                 await msg.pin()
