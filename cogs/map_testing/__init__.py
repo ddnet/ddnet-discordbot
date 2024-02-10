d0ea29f7527ab1d2e579ea3c7127c686df9f4521
murpi <60477660+murpii@users.noreply.github.com>
2024-02-10 16:34:56 +0100

    fix again

--- cogs/map_testing/__init__.py
+++ cogs/map_testing/__init__.py
@@ -656,7 +656,7 @@ class MapTesting(commands.Cog):
 
         thread = await channel.create_thread(name=f'Promote {trial_tester.global_name}', message=None, invitable=False)
         await thread.send(
-            f'<@&{ROLE_TESTER> \n'
+            f'<@&{ROLE_TESTER}> \n'
             f'{interaction.user.mention} suggests to promote {trial_tester.global_name} to Tester. Opinions?'
         )
 
